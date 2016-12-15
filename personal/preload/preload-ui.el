0dff338e5ce61a723731f3a5eb1f8a48874b428d
符风 <fufeng.syd@alipay.com>
2016-12-15 11:11:23 +0800

    add php mode and misc

--- personal/preload/preload-ui.el
+++ personal/preload/preload-ui.el
@@ -1,1 +1,4 @@
-(setq prelude-theme 'tsdh-dark)
+(add-to-list 'custom-theme-load-path (expand-file-name "zenburn-emacs" prelude-vendor-dir))
+;; (setq prelude-theme 'tango)
+(set-terminal-parameter nil 'background-mode 'dark)
+()
