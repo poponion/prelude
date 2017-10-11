df5f91eb350144d62e06b17442534af59e7751b0
符风 <fufeng.syd@alipay.com>
2017-10-11 17:21:40 +0800

    update

--- core/prelude-packages.el
+++ core/prelude-packages.el
@@ -41,6 +41,8 @@
              '("SC" . "http://joseito.republika.pl/sunrise-commander/") t)
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives
+             '("SC"   . "http://joseito.republika.pl/sunrise-commander/") t)
 
 ;; set package-user-dir to be relative to Prelude install path
 (setq package-user-dir (expand-file-name "elpa" prelude-dir))
