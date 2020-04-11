ae18397ae98bcac9faa8931a0959c168ee0b64e2
fangji.hcm <fangji.hcm@alipay.com>
2020-04-11 19:14:28 +0800

    add elpa packages

---
+++ elpa/lv-20200227.1301/lv-autoloads.el
@@ -0,0 +1,15 @@
+;;; lv-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("lv.el") (24205 39600 899013 360000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; lv-autoloads.el ends here
